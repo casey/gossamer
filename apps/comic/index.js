36a5505055480d73671778ece080bf703e296673
Casey Rodarmor <casey@rodarmor.com>
2024-06-30 19:35:16 -0700

    Error on mismatched content type

--- apps/comic/index.js
+++ apps/comic/index.js
@@ -5,6 +5,10 @@ class App {
   constructor() {
     let html = '';
 
+    if (manifest.type != "comic") {
+      document.body.innerHTML = `<h1>app cannot handle content type \`${manifest.type}\``;
+    }
+
     for (let i = 0; i < manifest.pages.length; i++) {
       if (i > 0) {
         html += '\n';
