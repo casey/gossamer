0e1b3602efe22265f71ebc4a03beccd590d88473
Casey Rodarmor <casey@rodarmor.com>
2024-07-13 00:41:25 -0700

    Simplify define function

--- crates/library/js/define.js
+++ crates/library/js/define.js
@@ -1,31 +1,10 @@
 export function define(name, callback) {
   class CustomElement extends HTMLElement {
-    static get observedAttributes() {
-    }
-
-    constructor() {
-      console.log('constructor');
-      super();
-    }
-
-    attributeChangedCallback(name, stale, fresh) {
-      console.log('attribute', name, stale, fresh);
-    }
-
     async connectedCallback() {
-      console.log('connected');
       let shadow = this.attachShadow({ mode: 'closed' });
       let [root, connected] = await callback;
       shadow.appendChild(root.documentElement);
       connected(shadow);
-    }
-
-    disconnectedCallback() {
-      console.log('disconnected');
-    }
-
-    adoptedCallback() {
-      console.log('adopted');
     }
   }
 
