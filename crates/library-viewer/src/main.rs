75f1a838ad8ebf34e1ffb5bca5cdaf2d56482331
Casey Rodarmor <casey@rodarmor.com>
2024-07-30 03:41:01 -0700

    More xilem

--- crates/library-viewer/src/main.rs
+++ crates/library-viewer/src/main.rs
@@ -36,18 +36,13 @@
 use {
   hypermedia::{
     log,
-    media::{Hash, Manifest, Target, Type},
+    media::{api, Hash, Manifest, Target, Type},
     wasm_bindgen::{self, prelude::wasm_bindgen, JsValue},
     Api,
   },
   std::collections::BTreeMap,
   xilem_web::{
-    concurrent::await_once,
-    core::fork,
-    document_body,
-    elements::html,
-    interfaces::{Element, HtmlButtonElement, HtmlDivElement, HtmlElement, HtmlLiElement},
-    AnyDomView, App, DomView,
+    concurrent::memoized_await, core::fork, elements::html, interfaces::Element, App, DomView,
   },
 };
 
@@ -55,92 +50,89 @@ use {
 struct State {
   handlers: BTreeMap<Target, Hash>,
   packages: Vec<(Hash, Manifest)>,
-  package: Option<(Hash, Hash)>,
-  origin: String,
+  selection: Option<Selection>,
+  node: Option<api::Node>,
+}
+
+#[derive(Debug)]
+enum Selection {
+  Package(Hash, Hash),
+  Node,
 }
 
 impl State {
   fn new() -> Self {
     Self {
       handlers: BTreeMap::new(),
+      node: None,
       packages: Vec::new(),
-      package: None,
-      origin: web_sys::window().unwrap().location().origin().unwrap(),
+      selection: None,
     }
   }
 
-  fn update(self: &mut State) -> impl DomView<State> {
+  fn package_list(self: &mut State, apps: bool) -> impl DomView<Self> {
     use html::*;
+    ul(
+      self
+        .packages
+        .iter()
+        .filter(|(_hash, manifest)| {
+          if apps {
+            manifest.ty() == Type::App
+          } else {
+            manifest.ty() != Type::App
+          }
+        })
+        .map(|(content, manifest)| {
+          li(
+            if let Some(handler) = self.handlers.get(&manifest.ty().into()) {
+              let handler = *handler;
+              let content = *content;
+              button(manifest.name.clone())
+                .on_click(move |state: &mut State, _| {
+                  state.selection = Some(Selection::Package(handler, content));
+                })
+                .boxed()
+            } else {
+              manifest.name.clone().boxed()
+            },
+          )
+        })
+        .collect::<Vec<_>>(),
+    )
+  }
 
-    log::debug!("{:?}", self);
+  fn update(self: &mut State) -> impl DomView<Self> {
+    use html::*;
 
     fork(
       div((
         nav((
           h1("System"),
           h1("Network"),
+          button("Node").on_click(|state: &mut State, _| {
+            state.selection = Some(Selection::Node);
+          }),
           h1("Apps"),
-          ul(
-            self
-              .packages
-              .iter()
-              .filter(|(_hash, manifest)| manifest.ty() == Type::App)
-              .map(|(content, manifest)| {
-                li(
-                  if let Some(handler) = self.handlers.get(&manifest.ty().into()) {
-                    log::debug!("{handler}");
-                    let handler = *handler;
-                    let content = *content;
-                    button(manifest.name.clone())
-                      .on_click(move |state: &mut State, _| {
-                        state.package = Some((handler, content));
-                      })
-                      .boxed()
-                  } else {
-                    manifest.name.clone().boxed()
-                  },
-                )
-              })
-              .collect::<Vec<_>>(),
-          ),
+          self.package_list(true),
           h1("Content"),
-          ul(
-            self
-              .packages
-              .iter()
-              .filter(|(_hash, manifest)| manifest.ty() != Type::App)
-              .map(|(content, manifest)| {
-                li(
-                  if let Some(handler) = self.handlers.get(&manifest.ty().into()) {
-                    log::debug!("{handler}");
-                    let handler = *handler;
-                    let content = *content;
-                    button(manifest.name.clone())
-                      .on_click(move |state: &mut State, _| {
-                        state.package = Some((handler, content));
-                      })
-                      .boxed()
-                  } else {
-                    manifest.name.clone().boxed()
-                  },
-                )
-              })
-              .collect::<Vec<_>>(),
-          ),
+          self.package_list(false),
         )),
-        main((
-          iframe(()).attr(
-            "src",
-            self
-              .package
-              .map(|(handler, content)| format!("{}/{handler}/{content}", self.origin)),
-          ),
-          section(()),
-        )),
+        main(self.selection.as_ref().map(|selection| {
+          match selection {
+            Selection::Package(handler, content) => Some(
+              iframe(())
+                .attr("src", format!("/{handler}/{content}/"))
+                .boxed(),
+            ),
+            Selection::Node => self.node.as_ref().map(|node| self.node(node).boxed()),
+          }
+        })),
       )),
       (
-        await_once(
-          |_| async {
+        memoized_await(
+          (),
+          |()| async {
             Api::default()
               .packages()
               .await
@@ -150,56 +142,66 @@ impl State {
           },
           |state: &mut State, output| state.packages = output,
         ),
-        await_once(
-          |_| async { Api::default().handlers().await.unwrap() },
+        memoized_await(
+          (),
+          |()| async { Api::default().handlers().await.unwrap() },
           |state: &mut State, output| state.handlers = output,
+        ),
+        memoized_await(
+          (),
+          |()| async { Api::default().node().await.unwrap() },
+          |state: &mut State, output| state.node = Some(output),
         ),
       ),
     )
   }
-}
 
-//     <nav>
-//       <h1>System</h1>
-//       <ul>
-//         <li><button id=node>Node</button></li>
-//       </ul>
-//       <h1>Network</h1>
-//       <ul>
-//         <li><button id=search>Search</button></li>
-//       </ul>
-//       <h1>Apps</h1>
-//       <ul>
-// %% for (hash, manifest) in self.packages.iter().filter(|(hash, manifest)| manifest.ty() == Type::App) {
-// %%   let ty = manifest.ty();
-//         <li>
-// %%   if let Some(handler) = self.handlers.get(&ty.into()) {
-//           <button class=package data-handler={{handler}} data-package={{hash}}>{{ manifest.name }}</button>
-// %%   } else {
-//           {{ manifest.name }}
-// %%   }
-//         </li>
-// %% }
-//       </ul>
-//       <h1>Content</h1>
-//       <ul>
-// %% for (hash, manifest) in self.packages.iter().filter(|(hash, manifest)| manifest.ty() != Type::App) {
-// %%   let ty = manifest.ty();
-//         <li>
-// %%   if let Some(handler) = self.handlers.get(&ty.into()) {
-//           <button class=package data-handler={{handler}} data-package={{hash}}>{{ manifest.name }}</button>
-//   %%   } else {
-//           {{ manifest.name }}
-// %%   }
-//         </li>
-// %% }
-//       </ul>
-//     </nav>
-//     <main>
-//       <iframe></iframe>
-//       <section></section>
-//     </main>
-//
+  fn node(&self, node: &api::Node) -> impl DomView<State> {
+    use html::*;
+
+    div((
+      h1("Node"),
+      node.peer.to_string(),
+      h2("Statistics"),
+      h3("Sent"),
+      node.sent,
+      h3("Received"),
+      node.received,
+      h2("Routing Table"),
+      ol(
+        node
+          .routing_table
+          .iter()
+          .enumerate()
+          .filter(|(_i, bucket)| !bucket.is_empty())
+          .map(|(i, bucket)| {
+            li(
+              bucket
+                .iter()
+                .map(|peer| span(peer.to_string()))
+                .collect::<Vec<_>>(),
+            )
+            .attr("value", i)
+          })
+          .collect::<Vec<_>>(),
+      ),
+      h2("Directory"),
+      node
+        .directory
+        .iter()
+        .map(|(hash, contacts)| {
+          li((
+            label(hash.to_string()),
+            contacts
+              .iter()
+              .map(|contact| span(contact.to_string()))
+              .collect::<Vec<_>>(),
+          ))
+        })
+        .collect::<Vec<_>>(),
+    ))
+  }
+}
 
 #[wasm_bindgen(main)]
 fn main() -> Result<(), JsValue> {
