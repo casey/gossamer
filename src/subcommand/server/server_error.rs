56b2a420c573067c98ed2b22fd69f1a2f58d5eae
Casey Rodarmor <casey@rodarmor.com>
2024-08-19 16:36:14 -0700

    Reform

--- src/subcommand/server/server_error.rs
+++ src/subcommand/server/server_error.rs
@@ -11,9 +11,8 @@ impl IntoResponse for ServerError {
   fn into_response(self) -> Response {
     match self {
       Self::NotFound { message } => (StatusCode::NOT_FOUND, message).into_response(),
-      Self::Node { source } => {
-        // todo: don't use debug representation
-        (StatusCode::INTERNAL_SERVER_ERROR, format!("{source:?}")).into_response()
+      Self::Node { .. } => {
+        (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error").into_response()
       }
     }
   }
