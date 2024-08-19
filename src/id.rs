c1566bb881a924e88538ad23b21efb060e11efe8
Casey Rodarmor <casey@rodarmor.com>
2024-08-19 15:33:24 -0700

    Adapt

--- src/id.rs
+++ src/id.rs
@@ -27,7 +27,6 @@ impl FromStr for Id {
   type Err = <blake3::Hash as FromStr>::Err;
 
   fn from_str(s: &str) -> Result<Self, Self::Err> {
-    // todo: don't use hash
     Ok(Self(*blake3::Hash::from_str(s)?.as_bytes()))
   }
 }
@@ -49,5 +48,27 @@ impl Serialize for Id {
     S: Serializer,
   {
     serde_bytes::ByteArray::new(self.0).serialize(serializer)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn serde() {
+    let id = Id::from([
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+      26, 27, 28, 29, 30, 31,
+    ]);
+
+    let mut expected = Vec::new();
+    expected.extend_from_slice(&[0x58, 32]);
+    expected.extend_from_slice(id.as_bytes());
+
+    assert_eq!(expected.len(), 34);
+
+    assert_eq!(id.to_cbor(), expected);
+    assert_eq!(Id::from_cbor(&expected).unwrap(), id);
   }
 }
